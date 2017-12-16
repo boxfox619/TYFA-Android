4f1ee8dcbfa44efe25824cfeddf38ac3a28f4866
dogyhbin2 <dogyhbin12345@gmail.com>
2017-12-16 22:17:51 +0900

    dogyhbin2 commit3

--- app/src/main/java/com/boxfox/appjam14application/Login2Activity.java
+++ app/src/main/java/com/boxfox/appjam14application/Login2Activity.java
@@ -1,28 +1,47 @@
 package com.boxfox.appjam14application;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by dsm2016 on 2017-12-16.
  */
 
 public class Login2Activity extends AppCompatActivity {
-    EditText login2_school_input, login2_grade_input, login2_class_input, login2_num_input;
+    RelativeLayout login2_login3_layout;
+    EditText login2_school_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        login2_num_input = findViewById(R.id.login2_num_input);
-        login2_class_input = findViewById(R.id.login2_class_input);
-        login2_grade_input = findViewById(R.id.login2_grade_input);
-        login2_school_input = findViewById(R.id.login2_school_input);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(login2_school_input, InputMethodManager.SHOW_IMPLICIT);
 
-        //학교 이름
-        // 학년 반 번호
+        login2_school_input = findViewById(R.id.login2_school_input);
+        login2_login3_layout = findViewById(R.id.login2_login3_layout);
+        login2_login3_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String school = login2_school_input.getText().toString();
+                if (school.equals("")) {
+                    Toast.makeText(getApplicationContext(),"학교를 입력해주세요.",Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(Login2Activity.this, Login3Activity.class);
+                    intent.putExtra("school_input", school);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/com/boxfox/appjam14application/MainActivity.java
+++ app/src/main/java/com/boxfox/appjam14application/MainActivity.java
@@ -1,19 +1,69 @@
 package com.boxfox.appjam14application;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 /**
  * Created by dsm2016 on 2017-12-16.
  */
-
+//모든 요청, 미리 결제된 요청, 현금 결제 요청, 대신 구매 요청
 public class MainActivity extends AppCompatActivity {
+
+    Spinner main_spinner;
+    String[] category = {"모든 요청", "미리 결제된 요청", "현금 결제 요청", "대신 구매 요청"};
+    String selected_category = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main_spinner = findViewById(R.id.main_spinner);
+        ArrayAdapter<String> dayAdapter = new ArrayAdapter<>(
+                getApplicationContext(), R.layout.main_spinner_item, category
+        );
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        main_spinner.setAdapter(dayAdapter);
+
+        main_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selected_category = (String) adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
+
+    public void onBackPressed() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage("정말 종료하시겠습니까?");
+        alertBuilder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        alertBuilder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertBuilder.show();
+    }
 }
