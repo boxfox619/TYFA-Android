de04301f861e3ebde98004bea72df71e25bf2cb1
dogyhbin2 <dogyhbin12345@gmail.com>
2017-12-16 16:00:31 +0900

    Commit1

---
+++ app/src/main/java/com/boxfox/appjam14application/MainActivity.java
@@ -0,0 +1,19 @@
+package com.boxfox.appjam14application;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dsm2016 on 2017-12-16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
