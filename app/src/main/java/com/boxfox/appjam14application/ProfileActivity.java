535ba77630c397494df509b2be9163a7b1fd93b5
rlatjdfo112 <rlatjdfo112@naver.com>
2017-12-16 17:26:24 +0900

    implement the profile image view

--- app/src/main/java/com/boxfox/appjam14application/ProfileActivity.java
+++ app/src/main/java/com/boxfox/appjam14application/ProfileActivity.java
@@ -2,7 +2,6 @@ package com.boxfox.appjam14application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.androidquery.AQuery;
@@ -10,17 +9,21 @@ import com.boxfox.appjam14application.data.UserData;
 
 public class ProfileActivity extends AppCompatActivity {
     private TextView tv_name, tv_school, tv_grade;
-    private ImageView iv_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        tv_name = findViewById(R.id.tv_name);
+        tv_school = findViewById(R.id.tv_school);
+        tv_grade = findViewById(R.id.tv_grade);
 
+        initUserProfile();
     }
 
-    private void initUserProfile(){
+
+    private void initUserProfile() {
         UserData data = UserData.getDefault();
         tv_name.setText(data.getName());
         tv_grade.setText(data.getGrade());
