c2661d9ceca17984d65be7821a6b03b544236937
rlatjdfo112 <rlatjdfo112@naver.com>
2017-12-16 16:03:21 +0900

    realm 연동, Splash activity 적용

---
+++ app/src/main/java/com/boxfox/appjam14application/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.boxfox.appjam14application;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.realm.Realm;
+
+/**
+ * Created by boxfox on 2017-12-16.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Realm.init(this);
+    }
+}
+
